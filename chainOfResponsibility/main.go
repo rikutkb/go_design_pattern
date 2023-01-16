744f55c9fa94898f7e9d368e76b5d9b369d4b302
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2023-01-16 21:29:49 +0900

    実行形式と他の型を追加

--- chainOfResponsibility/main.go
+++ chainOfResponsibility/main.go
@@ -12,53 +12,104 @@ func NewTrouble(number int) Trouble {
 func (t Trouble) GetNumber() int {
 	return t.number
 }
+func (t Trouble) String() string {
+	return fmt.Sprintf("%d", t.number)
+}
 
 type ISupport interface {
 	SetNext(next ISupport) ISupport
-	Support(t Trouble)
 	resolve(t Trouble) bool
-	done(t Trouble)
-	fail(t Trouble)
+	handle(t Trouble)
 }
-
-type NoSupport struct {
+type Support struct {
 	name string
+	own  ISupport
 	next ISupport
 }
 
-func NewNoSupport(name string) NoSupport {
-	return NoSupport{name: name}
-}
-func (ns *NoSupport) SetNext(next ISupport) ISupport {
-	ns.next = next
+func (s *Support) SetNext(next ISupport) ISupport {
+	s.next = next
 	return next
 }
 
-func (ns *NoSupport) Support(t Trouble) {
-	if ns.resolve(t) {
-		ns.done(t)
-	} else if ns.next != nil {
-		ns.next.Support(t)
+func (s *Support) handle(t Trouble) {
+	if s.own.resolve(t) {
+		s.done(t)
+	} else if s.next != nil {
+		s.next.handle(t)
 	} else {
-		ns.fail(t)
+		s.fail(t)
 	}
 }
 
+func (s *Support) done(t Trouble) {
+	fmt.Println(fmt.Sprintf("%s is resolved by %s", t, s))
+}
+
+func (s *Support) fail(t Trouble) {
+	fmt.Println(fmt.Sprintf("%s cannot be resolved", t))
+}
+func (s *Support) String() string {
+	return fmt.Sprintf("%s", s.name)
+}
+
+func (s *Support) resolve(t Trouble) bool {
+	return false
+}
+
+func NewSupport(name string) *Support {
+	return &Support{name: name}
+}
+
+type NoSupport struct {
+	*Support
+}
+
+func NewNoSupport(name string) *NoSupport {
+	noSupport := &NoSupport{Support: NewSupport(name)}
+	noSupport.Support.own = noSupport
+	return noSupport
+}
 func (ns *NoSupport) resolve(t Trouble) bool {
 	return false
 }
 
-func (ns *NoSupport) done(t Trouble) {
-	fmt.Println(fmt.Sprintln("%s is resolved by %s", t, ns))
+type OddSupport struct {
+	*Support
 }
 
-func (ns *NoSupport) fail(t Trouble) {
-	fmt.Println(fmt.Sprintln("%s cannot be resolved", t))
+func NewOddSupport(name string) *OddSupport {
+	oddSupport := &OddSupport{Support: NewSupport(name)}
+	oddSupport.Support.own = oddSupport
+	return oddSupport
 }
-func (ns NoSupport) String() string {
-	return fmt.Sprintf("[%s]", ns.name)
+func (os *OddSupport) resolve(t Trouble) bool {
+	return t.GetNumber()%2 == 0
+}
+
+type SpecialSupport struct {
+	*Support
+	number int
+}
+
+func (ss *SpecialSupport) resolve(t Trouble) bool {
+	return ss.number == t.GetNumber()
+}
+func NewSpecialSupport(name string, number int) *SpecialSupport {
+	specialSupport := &SpecialSupport{Support: NewSupport(name)}
+	specialSupport.number = number
+	specialSupport.Support.own = specialSupport
+	return specialSupport
 }
 
 func main() {
+	alice := NewNoSupport("Alice")
+	bob := NewOddSupport("Bob")
+	charlie := NewSpecialSupport("Charlie", 17)
+	elmo := NewSpecialSupport("Elmo", 19)
+	alice.SetNext(bob).SetNext(charlie).SetNext(elmo)
+	for i := 0; i < 20; i++ {
+		alice.handle(NewTrouble(i))
+	}
 
 }
